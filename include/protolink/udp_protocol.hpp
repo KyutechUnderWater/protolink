2b41cff5bced1232602b25799cc15914d19dc8a9
atomon <38009289+atomon@users.noreply.github.com>
2025-08-31 21:17:22 +0900

    feat: io_service -> io_context

--- include/protolink/udp_protocol.hpp
+++ include/protolink/udp_protocol.hpp
@@ -28,7 +28,7 @@ class Publisher
 {
 public:
   explicit Publisher(
-    boost::asio::io_service & io_service, const std::string & ip_address, const uint16_t port,
+    boost::asio::io_context & io_context, const std::string & ip_address, const uint16_t port,
     const uint16_t from_port, const rclcpp::Logger & logger = rclcpp::get_logger("protolink_udp"))
   : endpoint([ip_address, port]() {
       if (ip_address == "255.255.255.255") {
@@ -38,7 +38,7 @@ public:
         boost::asio::ip::address::from_string(ip_address), port);
     }()),
     logger(logger),
-    sock_(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), from_port))
+    sock_(io_context, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), from_port))
   {
     if (ip_address == "255.255.255.255" || ip_address.substr(ip_address.length() - 3, 3) == "255") {
       sock_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
@@ -68,15 +68,15 @@ class Subscriber
 {
 public:
   explicit Subscriber(
-    boost::asio::io_service & io_service, const uint16_t port,
+    boost::asio::io_context & io_context, const uint16_t port,
     std::function<void(const Proto &)> callback,
     const rclcpp::Logger & logger = rclcpp::get_logger("protolink_serial"))
   : logger(logger),
-    sock_(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)),
+    sock_(io_context, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)),
     callback_(callback)
   {
     start_receive();
-    io_service.run();
+    io_context.run();
   }
 
   void start_receive()
@@ -93,6 +93,7 @@ private:
   boost::asio::ip::udp::socket sock_;
   std::function<void(Proto)> callback_;
   boost::array<char, ReceiveBufferSize> receive_data_;
+
   void handler(const boost::system::error_code & error, size_t bytes_transferred)
   {
     if (error != boost::system::errc::success) {
