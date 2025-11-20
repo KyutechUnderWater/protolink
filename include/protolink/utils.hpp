3cb55b4b6f34132820c8c2656049be144e680798
atomon <38009289+atomon@users.noreply.github.com>
2025-11-20 15:37:24 +0900

    feat: add cobs

---
+++ include/protolink/utils.hpp
@@ -0,0 +1,84 @@
+// Copyright 2025 OUXT Polaris.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+namespace protolink
+{
+namespace utils
+{
+
+/**
+ * @brief Consistent Overhead Byte Stuffing(COBS)
+ * @details Remove 0x00 from the data and represent the packet terminator as 0x00.
+ */
+class Cobs
+{
+public:
+  static std::vector<uint8_t> encode(const std::vector<uint8_t> & input)
+  {
+    std::vector<uint8_t> output;
+    output.reserve(input.size() + input.size() / 254 + 2);
+
+    size_t code_idx = output.size();
+    output.push_back(0);  // placeholder
+    uint8_t code = 1;
+
+    for (uint8_t byte : input) {
+      if (byte != 0) {
+        output.push_back(byte);
+        code++;
+      }
+      if (byte == 0 || code == 0xFF) {
+        output[code_idx] = code;
+        code = 1;
+        code_idx = output.size();
+        if (byte == 0 || code == 0xFF) {
+          output.push_back(0);
+        }
+      }
+    }
+    output[code_idx] = code;
+    output.push_back(0x00);
+    return output;
+  }
+
+  static std::vector<uint8_t> decode(const uint8_t * data, size_t length)
+  {
+    std::vector<uint8_t> output;
+    output.reserve(length);
+    size_t index = 0;
+
+    while (index < length) {
+      uint8_t code = data[index];
+      index++;
+
+      // If only terminal 0x00
+      if (code == 0) break;
+
+      for (uint8_t i = 1; i < code; i++) {
+        if (index >= length) break;  // Error
+        output.push_back(data[index++]);
+      }
+      if (code < 0xFF && index < length) {
+        output.push_back(0);
+      }
+    }
+    return output;
+  }
+};
+}  // namespace utils
+}  // namespace protolink
